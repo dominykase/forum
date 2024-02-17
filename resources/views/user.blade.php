9ba43d9db0f94d1bf0d0415cd47df820665f365b
Dominykas <dominykas.eicinas@gmail.com>
2024-02-17 15:06:36 +0200

    changed profile page styling, added total post count

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -6,13 +6,17 @@
     <body style="width: 100vw;background-color: rgb(241 245 249);">
         <div class="w-full flex justify-center">
             <div class="mt-8 p-5 w-5/6 bg-slate-50 shadow-md">
-                <p>
-                    <span class="text-xl font-bold w-96">Username</span>
-                    <span class="text-md pl-5">{{ $user->name }}</span>
-                </p>
-                <p class="my-5">
-                    <span class="text-xl font-bold w-96">Posts</span>
-                </p>
+                <div class="flex flex-row items-center">
+                    <div class="text-xl font-bold w-32">Username</div>
+                    <div class="text-md pl-5">{{ $user->name }}</div>
+                </div>
+                <div class="flex flex-row items-center">
+                    <div class="text-xl font-bold w-32">Total posts</div>
+                    <div class="text-md pl-5">{{ $total }}</div>
+                </div>
+                <div class="my-3">
+                    <div class="text-xl font-bold w-96">Posts</div>
+                </div>
                 @foreach ($posts as $post)
                     <div class="bg-white p-4 m-4 shadow-md">
                         <p class="text-lg">{{ $post->thread->name }}</p>
